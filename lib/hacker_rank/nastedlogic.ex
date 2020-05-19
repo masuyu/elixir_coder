13eb4c22c862d31a0c1f104aa33d0dc90562bc3f
masuyu <masujazzcat@gmail.com>
2020-05-19 23:15:06 +0900

    add: nested another answer

--- lib/hacker_rank/nastedlogic.ex
+++ lib/hacker_rank/nastedlogic.ex
@@ -1,4 +1,5 @@
 defmodule NestedLogic do
+  # first answer
   def calc_fine(actual, expected) do
     cond do
       actual["y"] - expected["y"] >= 1 -> 10000
@@ -20,5 +21,20 @@ defmodule NestedLogic do
     actual = ["d", "m", "y"] |> Enum.zip(a) |> Map.new()
     expected = ["d", "m", "y"] |> Enum.zip(e) |> Map.new()
     calc_fine(actual, expected) |> IO.puts
+  end
+
+
+  # second answer
+  def calc_fine2([_, _, ay], [_, _, ey]) when ay - ey >= 1, do: 10000
+  def calc_fine2([_, _, ay], [_, _, ey]) when ay - ey < 0, do: 0
+  def calc_fine2([_, am, _], [_, em, _]) when am - em >= 1, do: (am - em) * 500
+  def calc_fine2([ad, _, _], [ed, _, _]) when ad - ed >= 1, do: (ad - ed) * 15
+  def calc_fine2([_, _, _], [_, _, _]), do: 0
+
+  def exec2 do
+    [ad, am, ay] = IO.gets("") |> String.trim |> String.split |> Enum.map(&String.to_integer/1)
+    [ed, em, ey] = IO.gets("") |> String.trim |> String.split |> Enum.map(&String.to_integer/1)
+
+    calc_fine2([ad, am, ay], [ed, em, ey]) |> IO.puts
   end
 end
